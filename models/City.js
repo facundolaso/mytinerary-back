5ff2f08cf8c370dadb1ea574fefa689920b6944a
facundolaso <lasofacundo@gmail.com>
2022-08-30 13:34:36 -0300

    se arreglo error(se cambio la s por mayúscula en Schema)

--- models/City.js
+++ models/City.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const schema = new mongoose.schema({
+const schema = new mongoose.Schema({
     city:{type: String, required: true},
     country:{type: String, required: true},
     photo:{type: String, required: true},
