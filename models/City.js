1624cb1e1c9b3f71c3f8f0e84e56d4837a4d9426
engelberthRetamal <engel.retamal.e@gmail.com>
2022-09-01 02:07:26 -0400

    Se agregó minimo y maximo a population

--- models/City.js
+++ models/City.js
@@ -4,7 +4,7 @@ const schema = new mongoose.Schema({
     city:{type: String, required: true},
     country:{type: String, required: true},
     photo:{type: String, required: true},
-    population:{type: Number, required: true},
+    population:{type: Number, required: true, min:1000, max:100000000},
     fundation:{type: Date, required: true},
 })
 
