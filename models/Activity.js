2670a159a9832fb1bbb68cdc12074cbf7329d6e5
engelberthRetamal <engel.retamal.e@gmail.com>
2022-09-06 18:45:51 -0300

    Se cambio el tipo de dato a itinerary

--- models/Activity.js
+++ models/Activity.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const schema = new mongoose.Schema({
     name:{type: String, required: true},
     photo:{type: String, required: true},
-    itinerary:{type: String}
+    itinerary:{type: mongoose.Types.ObjectId, ref:'itineraries'}
 })
 
 const Activity = mongoose.model(
