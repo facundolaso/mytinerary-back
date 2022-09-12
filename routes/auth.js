897771caabf6b6c0b586664a9f73f6e972b94c11
facundolaso <lasofacundo@gmail.com>
2022-09-12 20:28:09 -0300

    se agrego la ruta para verificar usuario

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 let express = require('express');
 let router = express.Router();
-let {create, signUp, updateUser} = require('../controllers/userController')
+let {create, signUp, updateUser, verifyMail} = require('../controllers/userController')
 
 
 router.post('/', create)
 router.post('/signup' ,signUp)
 router.patch('/:id', updateUser)
+router.get('/verification/:code',verifyMail)
 
 module.exports = router;
