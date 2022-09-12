c800388dfbffd5c6d57fcc8f2b822369649281f5
facundolaso <lasofacundo@gmail.com>
2022-09-12 18:10:30 -0300

    modificación de modelo user

--- models/User.js
+++ models/User.js
@@ -7,6 +7,11 @@ const schema = new mongoose.Schema({
     password:{type: String, required: true},
     photo:{type: String, required: true},
     country:{type: String, required: true},
+    from: [{type:String, required:true}], 
+    logged: {type:Boolean, required:true}, 
+    verified: {type:Boolean, required:true}, 
+    code: {type:String, required:true},
+    role: {type:String, required:true}, 
 })
 
 const User = mongoose.model(
