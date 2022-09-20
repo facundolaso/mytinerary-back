de14d0dced399c0bb4c61d3d8f0bafa8111c235d
facundolaso <lasofacundo@gmail.com>
2022-09-20 16:54:59 -0300

    fix de ruta de comment create

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let router = express.Router();
 let {create, readAll} = require('../controllers/commentController')
+let passport = require('../config/passport')
 
 
 router.post('/', passport.authenticate('jwt', {session:false}), create)
