f669470e7c6a8f7bae5ec6e6c2a25df2d83f2387
facundolaso <lasofacundo@gmail.com>
2022-09-21 11:08:46 -0300

    arreglo de ruta

--- routes/comments.js
+++ routes/comments.js
@@ -1,10 +1,11 @@
 let express = require('express');
 let router = express.Router();
-let {create, readAll} = require('../controllers/commentController')
+let {create, readAll, updateComment} = require('../controllers/commentController')
 let passport = require('../config/passport')
 
 
 router.post('/', passport.authenticate('jwt', {session:false}), create)
+// router.patch('/', passport.authenticate('jwt', {session:false}), updateComment)
 router.get('/', readAll)
 
 module.exports = router;
